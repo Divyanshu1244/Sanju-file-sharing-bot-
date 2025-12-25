a23264a8a7395781ef55f91ac308dcd0737ea4cb
Divyanshu1244 <126379435+Divyanshu1244@users.noreply.github.com>
2025-12-25 23:41:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')  # Replace with your real token if hardcoding
 ADMIN_IDS = [7078689012]  # Add more if needed, e.g., [7078689012, another_id]
 
 # Channel IDs (use @userinfobot or bot API to confirm)
-MAIN_CHANNEL_ID = 't.me/+DIfAWc7sGfk2MDk1'  # e.g., '@MyMainChannel'
-PRIVATE_CHANNEL_ID = 't.me/+ekRzaY45ZrM0ZGRl'  # e.g., '@MyPrivateChannel'
+MAIN_CHANNEL_ID = 't+DIfAWc7sGfk2MDk1'  # e.g., '@MyMainChannel'
+PRIVATE_CHANNEL_ID = 't+ekRzaY45ZrM0ZGRl'  # e.g., '@MyPrivateChannel'
 DB_CHANNEL_ID = -1003527164888  # DB channel for storing forwarded files
 LOGGER_CHANNEL_ID = -1003609332664  # Logger channel for user access logs
 
